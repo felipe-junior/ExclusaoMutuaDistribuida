f4243f3136d37e35126f9bd9b04b761eb5f0f580
Vinícius Leitão <vinileitao2000@gmail.com>
2022-07-22 01:49:35 -0300

    Logging, Múltiplos processos e parte da lógica implementada

--- app.py
+++ app.py
@@ -1,16 +1,89 @@
 import asyncio
 from websockets import server
+from threading import Thread
+import logging
+from datetime import datetime
+
+
+logging.basicConfig(filename='controlador.log',
+                    # w -> sobrescreve o arquivo a cada log
+                    # a -> não sobrescreve o arquivo
+                    filemode='a',
+                    level=logging.INFO)
+logger = logging.getLogger('root')
+
+
+def desfazMensagem(message):
+    mensagem, processo, lixo = message.split('|')
+    if mensagem == '1':
+        mensagem = 'REQUEST'
+    else:
+        mensagem = 'RELEASE'
+
+    return mensagem, processo, lixo
+
 
 async def echo(websocket: server.WebSocketServerProtocol):
+    fila = []
+    # Toda vez que dermos um grant para um processo, temos que salvar seu id para contar e saber quantas vezes ele acessou. Ou algo parecido com isso, mas temos que saber quantas vezes cada processo acessou a região crítica através do terminal.
     async for message in websocket:
-        websocket.id
 
-        print("Hello")
-        websocket.
-        await websocket.send(message)
+        mensagem, processo, lixo = desfazMensagem(message)
+        logger.info(
+            f'TIMESTAMP: {datetime.today()} TIPO DE MENSAGEM: {mensagem} NUMERO DO PROCESSO: {processo}')
+
+        # Lógica do controle de fila tá zoado. To cheio de sono, rapaziada. Espero ter adiantado um pouco.
+        if message.split('|')[0] == str(1):
+            if len(fila) == 0:
+                fila.append({'ID_Thread': message.split(
+                    '|')[1], 'Socket': websocket})
+                await websocket.send("GRANT")
+            if len(fila) > 0:
+                fila.append({'ID_Thread': message.split(
+                    '|')[1], 'Socket': websocket})
+        else:
+            del fila[0]
+            # Eu não sei se isso aqui ta sendo efetivo. Tentei sobrescrever o socket com o primeiro da fila toda vez que eu recebo um release. Para dar grant a outro processo. Mas n sei se essa desgraça tá funcionando.
+            websocket = fila[0]['Socket']
+            await websocket.send("GRANT")
+
+        # Lógica da fila tá toda torta, conseguem ajeitar? Cheio de sono.
+
 
 async def main():
     async with server.serve(echo, "localhost", 8765):
         await asyncio.Future()  # run forever
+
+# Thread para o controle de fila, não soube fazer, como dito acima
+
+
+def controle_de_fila():
+    print('Algum controle de fila aqui nessa thread. Codar')
+
+# Aqui é uma thread para o terminal, depende do código do controle de fila para impressão da fila e da quantidade de vezes que determinada thread acessou a região crítica. Mas a lógica segue essa aí.
+
+
+def terminal():
+    while True:
+        input_thread = int(input(
+            'O que você precisa?\n1. Imprimir fila de pedidos atual\n2. Imprimir quantas vezes cada processo foi atendido\n3. Encerrar execução\n'))
+        if input_thread == 1:
+            print('Imprimindo fila')
+        elif input_thread == 2:
+            print('Imprimindo quantidade')
+        elif input_thread == 3:
+            print('Encerrando execução.')
+            break
+        else:
+            continue
+
+
+# Instanciando e iniciando a thread do terminal
+thread_terminal = Thread(target=terminal)
+thread_terminal.start()
+
+# Instanciando e iniciando a thread do controle de fila
+thread_controle_de_fila = Thread(target=controle_de_fila)
+thread_controle_de_fila.start()
 
 asyncio.run(main())
---
+++ main.py
@@ -0,0 +1,60 @@
+import asyncio
+from websockets import client
+import multiprocessing
+import time
+from datetime import datetime
+
+# Definindo os ids dos comandos abaixo
+request = 1
+release = 2
+# Setando o tamanho da string
+F = 10
+# Número de vezes que o processo vai solicitar para entrar na região crítica
+r = 5
+# Tempo em segundos que o processo vai dormir após entrar na região crítica
+k = 3
+
+# Função que retorna a mensagem no formato desejado com separadores, indentificadores de Thread e Comando
+
+
+def retornaMensagemFormatada(type, id):
+    string = str(type) + '|' + str(id) + '|'
+    tamanho_mensagem = len(string)
+    if tamanho_mensagem < 10:
+        string = string + (10 - tamanho_mensagem) * '0'
+    return string
+
+
+def createClient(id):
+    async def hello():
+        async with client.connect("ws://localhost:8765") as websocket:
+            for i in range(r):
+                # Chamando a função para formatar a mensagem, passando o comando e o id da thread.
+                mensagem = retornaMensagemFormatada(request, id)
+                await websocket.send(mensagem)
+                try:
+                    # Aguardando receber o GRANT do controlador
+                    await websocket.recv()
+                    # Registrando o id do processo e o timestamp
+                    with open('resultado.txt', 'a') as res:
+                        res.write(
+                            f'Processo {id} - Timestamp: {datetime.today()}\n')
+                    # Sleep no código só para fins de "fiz alguma coisa e saí"
+                    time.sleep(k)
+                    # Retorna a mensagem de release para o controlador
+                    mensagem = retornaMensagemFormatada(release, id)
+                    await websocket.send(mensagem)
+                except client.ClientConnection.close_expected:
+                    print(f'Terminated')
+                    break
+    asyncio.run(hello())
+
+
+# Instanciando os PROCESSOS 1 e 2
+
+if __name__ == "__main__":
+    p1 = multiprocessing.Process(target=createClient, args=(1,))
+    p2 = multiprocessing.Process(target=createClient, args=(2,))
+
+    p1.start()
+    p2.start()
